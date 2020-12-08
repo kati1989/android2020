a1c99a124b7812fda051fdadb117f34b268ef1f0
kati1989 <56889441+kati1989@users.noreply.github.com>
2020-12-09 00:04:23 +0200

    added navigation, profile fragment

--- FoodFinder/app/src/main/java/com/example/foodfinder/MainActivity.kt
+++ FoodFinder/app/src/main/java/com/example/foodfinder/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.example.foodfinder
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 
 class MainActivity : FragmentActivity() {
 
@@ -14,7 +19,27 @@ class MainActivity : FragmentActivity() {
         foodDb = FoodDatabase.getInstance(context = this)
         setContentView(R.layout.activity_main)
 
+        // set desired fragment for the first time
+        // set desired fragment for the first time
+        val fm: FragmentManager = supportFragmentManager
+        val ft: FragmentTransaction = fm.beginTransaction()
 
+        ft.add(R.id.fragment_placeHolder, RestaurantList())
+        ft.commit()
+    }
 
+    fun switch_fragment(view: View) {
+        var newFragment: Fragment? = null
+        when (view.id) {
+            R.id.btn_1 -> newFragment = EditProfile()
+            R.id.btn_2 -> newFragment = RestaurantList()
+        }
+        setFragment(newFragment)
+    }
+
+    fun setFragment(fragment: Fragment?) {
+        val fragmentTransaction: FragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_placeHolder, fragment!!)
+        fragmentTransaction.commit()
     }
 }
--- FoodFinder/app/src/main/java/com/example/foodfinder/RestaurantList.kt
+++ FoodFinder/app/src/main/java/com/example/foodfinder/RestaurantList.kt
@@ -43,4 +43,6 @@ class RestaurantList : Fragment() {
     companion object {
         fun newInstance(): RestaurantList = RestaurantList()
     }
+
+
 }
